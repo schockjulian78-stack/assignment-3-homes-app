b6154899b945754607212b771d278ab3148f28ae
Julian Schock <schockjulian78@gmail.com>
2026-03-18 19:53:54 -0500

    integrated details page into the application

---
+++ src/app/details/details.ts
@@ -0,0 +1,21 @@
+import {Component, inject} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HousingService} from '../housing-service';
+import {HousingLocationInfo} from '../housinglocationinfo';
+
+@Component({
+  selector: 'app-details',
+  imports: [],
+  templateUrl: './details.html',
+  styleUrl: './details.css',
+})
+export class Details {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
+  housingService = inject(HousingService);
+  housingLocation : HousingLocationInfo | undefined;
+  constructor() {
+  this.housingLocationId = Number(this.route.snapshot.params['id']);
+  this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
+}
