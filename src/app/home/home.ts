f4b32ee5bf12df180494c664ee4ee392ae99259c
Julian Schock <schockjulian78@gmail.com>
2026-03-18 20:05:20 -0500

    added the search feature to the angular app

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,21 +1,32 @@
 import { Component, inject } from '@angular/core';
 import { HousingLocation } from '../housing-location/housing-location';
-import {HousingLocationInfo} from '../housinglocationinfo';
-import {HousingService} from '../housing-service';
+import { HousingLocationInfo } from '../housinglocationinfo';
+import { HousingService } from '../housing-service';
 
 @Component({
   selector: 'app-home',
   imports: [HousingLocation],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home{
-  // readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+export class Home {
   housingLocationList: HousingLocationInfo[] = [];
-  // Injecting HousingService into the Component
   housingService: HousingService = inject(HousingService);
-  constructor(){
-  //setting housingLocationlist to the data array in HousingService
-  this.housingLocationList = this.housingService.getAllHousingLocations();
+  filteredLocationList: HousingLocationInfo[] = [];
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
+}
